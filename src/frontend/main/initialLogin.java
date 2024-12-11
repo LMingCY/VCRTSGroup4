498ea55633f3b1198eeed4a37f12f9dadf3b2522
LMingCY <leonmingmcy@gmail.com>
2024-12-10 19:42:58 -0500

    deleted second welcome

--- src/frontend/main/initialLogin.java
+++ src/frontend/main/initialLogin.java
@@ -38,7 +38,7 @@ public class initialLogin extends JFrame {
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 18));
         welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        generalWelcomeMessage = new JLabel("<html><div style='text-align:center;'>Welcome to our VCRTS!<br>Use our features to either rent out your vehicle or post a job!</div></html>");
+        generalWelcomeMessage = new JLabel("<html><div style='text-align:center;'><br>Use our features to either rent out your vehicle or post a job!</div></html>");
         generalWelcomeMessage.setFont(new Font("Arial", Font.PLAIN, 14));
 
         loginPromptLabel = new JLabel("Please login:");
